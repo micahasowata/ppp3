2e19ccf4e7c2c2daa3857fd803aa759e0dbed631
Micah Asowata <micahasowata@gmail.com>
2026-01-15 18:12:41 +0000

    testing string concatenation

--- chapter_two/operators.cpp
+++ chapter_two/operators.cpp
@@ -1,16 +1,16 @@
 // This program should highlight how types determine operators and their
 // operations
-#include <cmath>
 #include <iostream>
+#include <string>
 
 int main() {
-  std::cout << "Please enter an integer value:\n";
-  int n = 0;
-  std::cin >> n;
+  std::cout << "Please enter your first and last names\n";
+  std::string first_name;
+  std::string last_name;
 
-  std::cout << "n == " << n << "\nn + 1 = " << n + 1
-            << "\nthree times n = " << 3 * n << "\ntwice n = " << n * 2
-            << "\nn modulo 2 = " << n % 2 << "\nhalf of n = " << n / 2
-            << "\nn squared = " << n * n
-            << "\nsquare root of n = " << std::sqrt(n) << "\n";
+  std::cin >> first_name >> last_name;
+
+  std::string full_name = first_name + ' ' + last_name;
+
+  std::cout << "Hello, " << full_name << '\n';
 }
