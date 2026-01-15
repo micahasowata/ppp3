e844b23dc0ae9a2b2b7e234767fd4a565c6acc05
Micah Asowata <micahasowata@gmail.com>
2026-01-15 14:57:27 +0000

    the major types and their variable declarations

---
+++ chapter_two/variables.cpp
@@ -0,0 +1,20 @@
+// This program should show variable declaration
+#include <iostream>
+#include <string>
+
+int main() {
+  int number_of_steps = 39;
+  std::cout << number_of_steps << "\n";
+
+  double flying_time = 3.5;
+  std::cout << flying_time << "\n";
+
+  char decimal_point = '.';
+  std::cout << decimal_point << "\n";
+
+  std::string name = "Zuckates";
+  std::cout << name << "\n";
+
+  bool tap_on = true;
+  std::cout << tap_on << "\n";
+}
