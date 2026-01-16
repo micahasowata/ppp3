bf9eee8b5da0d8bed69e764521d93bdd8ff79445
Micah Asowata <micahasowata@gmail.com>
2026-01-16 01:03:36 +0000

    update: add a word counter to direct user to repeated word(s)

--- chapter_two/operators.cpp
+++ chapter_two/operators.cpp
@@ -3,13 +3,17 @@
 #include <string>
 
 int main() {
+  int word_count = 0;
+
   std::cout << "Write a sentence with some repeated words\n";
   std::string previous;
   std::string current;
 
   while (std::cin >> current) {
+    ++word_count;
     if (previous == current) {
-      std::cout << "Repeated word: " << current << "\n";
+      std::cout << "word number " << word_count << ": " << current
+                << " is repeated." << "\n";
     }
 
     previous = current;
