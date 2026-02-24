862ee6d633474191ebc72254aed8b320e7742dcc
Micah Asowata <micahasowata@gmail.com>
2026-02-24 16:17:48 +0000

    find line(s) where `pattern` occurs in file `path`

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use clap::Parser;
-use std::path;
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader, BufWriter, Write},
+    path,
+};
 
 #[derive(Parser)]
 struct Cli {
@@ -10,5 +14,26 @@ struct Cli {
 fn main() {
     let args = Cli::parse();
 
-    println!("pattern {:?}, path: {:?}", args.pattern, args.path);
+    let file = File::open(&args.path);
+
+    let file = match file {
+        Ok(file) => file,
+        Err(error) => panic!("error opening file {error:?}"),
+    };
+
+    let stdout = io::stdout();
+    let mut out = BufWriter::new(stdout.lock());
+
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        let line = match line {
+            Ok(line) => line,
+            Err(err) => panic!("error reading line: {err:?}"),
+        };
+
+        if line.contains(&args.pattern) {
+            writeln!(out, "{line}").expect("");
+        }
+    }
 }
