a04cebeab613b8eebccc8a3ac0678210b4e4b414
Micah Asowata <micahasowata@gmail.com>
2026-02-24 12:37:19 +0000

    get user input as args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::{env, path};
+
+struct Cli {
+    pattern: String,
+    path: path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let pattern = env::args().nth(1).expect("no pattern given");
+    let path = env::args().nth(2).expect("no path given");
+
+    let args = Cli {
+        pattern,
+        path: path::PathBuf::from(path),
+    };
+
+    println!("pattern {:?}, path: {:?}", args.pattern, args.path);
 }
