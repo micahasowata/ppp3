9cab7a09b6ea953986308874a00a2a7111bbd201
Micah Asowata <micahasowata@gmail.com>
2026-03-03 11:46:42 +0000

    add hello world from tour

--- c1/hello_world.cpp
+++ c1/hello_world.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 
-int main() // C++ programs start by executing the function main
-{
-    std::cout << "Hello, World!\n"; // output "Hello, World!"
-    return 0;
+int main() {
+    std::cout << "Hello, World!\n";
 }
